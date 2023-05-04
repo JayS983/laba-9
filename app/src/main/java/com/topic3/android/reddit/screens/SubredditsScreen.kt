379d3342eeebc5574ecc51684554a41fb7bfcf67
JayS <krazynik016@gmail.com>
2023-05-04 17:29:22 +0300

    4

--- app/src/main/java/com/topic3/android/reddit/screens/SubredditsScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/SubredditsScreen.kt
@@ -1,5 +1,7 @@
 package com.topic3.android.reddit.screens
 
+import android.graphics.Paint.Align
+import android.widget.Space
 import androidx.annotation.StringRes
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
@@ -28,7 +30,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintLayoutBaseScope
 import com.topic3.android.reddit.R
+import com.topic3.android.reddit.components.BackgroundText
 import com.topic3.android.reddit.models.SubredditModel
 
 val subreddits = listOf(
@@ -68,18 +72,95 @@ val communities = listOf(
 
 @Composable
 fun SubredditsScreen(modifier: Modifier = Modifier) {
-    //TODO add your code here
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+    ){
+        Text(
+            modifier=modifier.padding(16.dp),
+            text= stringResource(R.string.recently_visited_subreddits),
+            fontSize = 12.sp,
+            style=MaterialTheme.typography.subtitle1
+        )
+
+        LazyRow(
+            modifier = modifier.padding(end=16.dp)
+        ){
+            items(subreddits){ Subreddit(it)}
+        }
+        Communities(modifier)
+    }
 }
 
 @Composable
 fun Subreddit(subredditModel: SubredditModel, modifier: Modifier = Modifier) {
-    //TODO add your code here
+    Card(
+        backgroundColor = MaterialTheme.colors.onSurface,
+        shape= RoundedCornerShape(4.dp),
+        modifier = modifier
+            .size(120.dp)
+            .padding(start = 2.dp, end = 2.dp, top = 4.dp, bottom = 4.dp)
+    ){
+        SubredditBody(subredditModel)
+    }
 }
 
 @Composable
 fun SubredditBody(subredditModel: SubredditModel, modifier: Modifier = Modifier) {
-    //TODO add your code here
+    ConstraintLayout(
+        modifier= modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colors.surface)
+    ) {
+        val (backgroundImage,icon,name,members,description)=createRefs()
+
+        SubredditImage(
+            modifier = modifier.constrainAs(backgroundImage){
+                centerHorizontallyTo(parent)
+                top.linkTo(parent.top)
+            }
+        )
+
+        SubredditIcon(
+            modifier = modifier
+                .constrainAs(icon) {
+                    top.linkTo(backgroundImage.bottom)
+                    bottom.linkTo(backgroundImage.bottom)
+                    centerHorizontallyTo(parent)
+                }
+                .zIndex(1f)
+        )
+
+        SubredditName(
+            nameStringRes = subredditModel.nameStringRes,
+            modifier = modifier.constrainAs(name){
+                top.linkTo(icon.bottom)
+                centerHorizontallyTo(parent)
+
+            }
+        )
+        SubredditMembers(
+            membersStringRes = subredditModel.membersStringRes,
+            modifier = modifier.constrainAs(members){
+                top.linkTo(name.bottom)
+                centerHorizontallyTo(parent)
+            }
+        )
+        SubredditDescription(
+            descriptionStringRes = subredditModel.descriptionStringRes,
+            modifier = modifier.constrainAs(description){
+                top.linkTo(members.bottom)
+                centerHorizontallyTo(parent)
+            }
+
+        )
+
+
+
+    }
 }
+
+
 
 @Composable
 fun SubredditImage(modifier: Modifier) {
@@ -134,13 +215,44 @@ fun SubredditDescription(modifier: Modifier, @StringRes descriptionStringRes: In
 }
 
 @Composable
-fun Community(text: String, modifier: Modifier = Modifier) {
-    //TODO add your code here
+fun Community(
+    text: String, modifier: Modifier = Modifier,
+    onCommunityClicked:()->Unit={}) {
+    Row(modifier = modifier
+        .padding(start = 16.dp, top = 16.dp)
+        .fillMaxWidth()
+        .clickable { onCommunityClicked.invoke() }){
+        Image(
+            bitmap= ImageBitmap.imageResource(id = R.drawable.subreddit_placeholder),
+            contentDescription = stringResource(id = R.string.community_icon),
+            modifier = modifier
+                .size(24.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            fontSize = 10.sp,
+            color=MaterialTheme.colors.primaryVariant,
+            text=text,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+                .padding(start = 16.dp)
+                .align(Alignment.CenterVertically)
+        )
+    }
+
 }
 
 @Composable
 fun Communities(modifier: Modifier = Modifier) {
-    //TODO add your code here
+    mainCommunities.forEach{
+        Community(text = stringResource(it))
+        Spacer(modifier=modifier.height(4.dp))
+        BackgroundText(stringResource(R.string.communities))
+        communities.forEach{
+            Community(text = stringResource(it))
+        }
+
+    }
 }
 
 @Preview
